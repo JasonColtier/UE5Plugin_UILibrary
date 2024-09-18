f3adafe7b282ab3df1d0973b163f508e35819fea
Jason <jasoncoltier@gmail.com>
2024-09-18 09:47:52 +0200

    add tool for handeling string table (WIP at the moment)

---
+++ Source/UILibrary/Private/BPFL_UI.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BPFL_UI.h"
+
+#include "Internationalization/StringTable.h"
+#include "Internationalization/StringTableCore.h"
+#include "Internationalization/StringTableRegistry.h"
+
+void UBPFL_UI::AddTextToStringTable(const FName TableId, const FString Key, const FString String)
+{
+	FStringTablePtr StringTable = FStringTableRegistry::Get().FindMutableStringTable(TableId);
+	if (StringTable.IsValid())
+	{
+		StringTable->SetSourceString(Key, String);
+		StringTable->GetOwnerAsset()->MarkPackageDirty();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Could not find string table for id : %s"),*TableId.ToString());
+	}
+}
---
+++ Source/UILibrary/Public/BPFL_UI.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPFL_UI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UILIBRARY_API UBPFL_UI : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static void AddTextToStringTable(const FName TableId, const FString Key,const FString String);
+};
